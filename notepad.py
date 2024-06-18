bcfecb73c22fb89954248851074c4cd5477587c8
Md Shishir <mdshishirr66@gmail.com>
2024-06-19 02:04:35 +0600

    Day 1

---
+++ notepad.py
@@ -0,0 +1,38 @@
+
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import  font, colorchooser, filedialog, messagebox
+
+import  os
+
+main_application = tk.Tk()
+main_application.geometry("800x600")
+main_application.title("MS Notepad")
+
+main_menu = tk.Menu()
+
+new_icon = tk.PhotoImage(file="icons/new_folder.png")
+open_icon = tk.PhotoImage(file="icons/open.png")
+save_icon = tk.PhotoImage(file="icons/save_folder.png")
+save_as_icon = tk.PhotoImage(file="icons/save_folder.png")
+exit_icon = tk.PhotoImage(file="icons/exit.png")
+
+file = tk.Menu(main_menu, tearoff=False)
+main_menu.add_cascade(label="File", menu=file)
+main_application.config(menu=main_menu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main_application.mainloop()
